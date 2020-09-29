6db8e660a0142b35218e677efdaa03e2cc8ce620
Harry Collet <harrison.collet@wsu.edu>
2020-09-29 11:32:36 -0700

    Added test framework NUnit project to solution

--- ExpenseTrackerEngine/Properties/AssemblyInfo.cs
+++ ExpenseTrackerEngine/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿// <copyright file="AssemblyInfo.cs" company="Harrison Collet">
+// Copyright (c) Harrison Collet. All rights reserved.
+// </copyright>
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
---
+++ ExpenseTrackerEngineTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+// <copyright file="AssemblyInfo.cs" company="Harrison Collet">
+// Copyright (c) Harrison Collet. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExpenseTrackerEngineTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExpenseTrackerEngineTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8d7ab85-ba6e-49e5-8280-714d6705ac26")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
